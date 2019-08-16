7db9927dcd4f22d28b05aa2e2b001e302e7efa22
K. Adam White <kadamwhite@users.noreply.github.com>
2019-08-16 15:36:08 -0400

    Add argent theme at v1.1.5

---
+++ inc/jetpack-fonts.php
@@ -0,0 +1,244 @@
+<?php
+
+add_filter( 'typekit_add_font_category_rules', function( $category_rules ) {
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'body,
+		button,
+		input,
+		select,
+		textarea',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Cabin", Helvetica, sans-serif' ),
+			array( 'property' => 'font-size', 'value' => '20px' ),
+			array( 'property' => 'font-weight', 'value' => '500' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'h1,
+		h2,
+		h3,
+		h4,
+		h5,
+		h6',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Alegreya", Georgia, serif' ),
+			array( 'property' => 'font-weight', 'value' => '700' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'h1',
+		array(
+			array( 'property' => 'font-size', 'value' => '40px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'h2',
+		array(
+			array( 'property' => 'font-size', 'value' => '36px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'h3',
+		array(
+			array( 'property' => 'font-size', 'value' => '32px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'h4',
+		array(
+			array( 'property' => 'font-size', 'value' => '24px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'h5,
+		h6',
+		array(
+			array( 'property' => 'font-size', 'value' => '18px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'blockquote:not(.contact-form-submission)',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Alegreya", Georgia, serif' ),
+			array( 'property' => 'font-size', 'value' => '40px' ),
+			array( 'property' => 'font-style', 'value' => 'italic' ),
+			array( 'property' => 'font-weight', 'value' => '700' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'blockquote:not(.contact-form-submission) cite',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Cabin", Helvetica, sans-serif' ),
+			array( 'property' => 'font-size', 'value' => '16px' ),
+			array( 'property' => 'font-style', 'value' => 'normal' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'none',
+		'pre',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Courier 10 Pitch", Courier, monospace' ),
+			array( 'property' => 'font-size', 'value' => '14px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'none',
+		'code,
+		kbd,
+		tt,
+		var',
+		array(
+			array( 'property' => 'font-family', 'value' => 'Monaco, Consolas, "Andale Mono", "DejaVu Sans Mono", monospace' ),
+			array( 'property' => 'font-size', 'value' => '15px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'none',
+		'.menu-toggle:before',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Genericons"' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'.widget-title',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Cabin", Helvetica, sans-serif' ),
+			array( 'property' => 'font-size', 'value' => '20px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'.site-title',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Cabin", Helvetica, sans-serif' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'.site-description',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Cabin", Helvetica, sans-serif' ),
+			array( 'property' => 'font-size', 'value' => '16px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'.entry-title',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Cabin", Helvetica, sans-serif' ),
+			array( 'property' => 'font-size', 'value' => '18px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'.page-title,
+		.page-title,
+		.single .entry-title',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Alegreya", Georgia, serif' ),
+			array( 'property' => 'font-size', 'value' => '40px' ),
+			array( 'property' => 'font-size', 'value' => '4rem' ),
+			array( 'property' => 'font-style', 'value' => 'italic' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'headings',
+		'.project-title',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Cabin", Helvetica, sans-serif' ),
+			array( 'property' => 'font-size', 'value' => '26px' ),
+			array( 'property' => 'font-size', 'value' => '2.6rem' ),
+			array( 'property' => 'font-weight', 'value' => '700' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'.main-navigation a',
+		array(
+			array( 'property' => 'font-size', 'value' => '15px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'.main-navigation a',
+		array(
+			array( 'property' => 'font-size', 'value' => '12px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'.entry-content',
+		array(
+			array( 'property' => 'font-size', 'value' => '16px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'.single .entry-content,
+		.page .page-content',
+		array(
+			array( 'property' => 'font-size', 'value' => '18px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'.read-more ',
+		array(
+			array( 'property' => 'font-size', 'value' => '12px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'.entry-meta,
+		.site-info',
+		array(
+			array( 'property' => 'font-size', 'value' => '13px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'.wp-caption-text,
+		.comment-metadata',
+		array(
+			array( 'property' => 'font-size', 'value' => '14px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'.site-main .comment-navigation,
+		.site-main .posts-navigation,
+		.site-main .post-navigation',
+		array(
+			array( 'property' => 'font-size', 'value' => '14px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'.widget-area,
+		.widget-area a,
+		#comments',
+		array(
+			array( 'property' => 'font-size', 'value' => '16px' ),
+		)
+	);
+
+	TypekitTheme::add_font_category_rule( $category_rules, 'body-text',
+		'div.sharedaddy h3.sd-title,
+		div#jp-relatedposts h3.jp-relatedposts-headline ',
+		array(
+			array( 'property' => 'font-family', 'value' => '"Cabin", Helvetica, sans-serif' ),
+		)
+	);
+
+	return $category_rules;
+} );
